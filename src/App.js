900c3b1bdf2948e8d880d35ee329e0a6ec0956ab
keshridivya <dkeshari094@gmail.com>
2022-08-10 18:35:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import UseState from './component/Hooks/UseState';
+
+const  App = () => {
+  return <UseState/>
+};
+
+export default App;
---
+++ src/component/Hooks/UseState.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './style.css';
+
+const useState = () => {
+   const initialCount = 10;
+  const [num,setNum]= React.useState(initialCount);
+  return (
+    <div className="center_div">
+      <p>{num}</p>
+      <div className="button2" onMouseDown={() => setNum(num+1)} onClick={() => setNum(num+1)}>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Increment
+      </div>
+      <div className="button2" onMouseDown={() => setNum(num-1)} onClick={() => setNum(num-1)}>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Decrement
+      </div>
+    </div>
+  )
+}
+
+export default useState
